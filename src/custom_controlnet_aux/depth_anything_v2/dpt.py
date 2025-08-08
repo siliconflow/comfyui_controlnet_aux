eb0bfdc8da7ad320e8a1f02d66fd051d18a713a4
Ldpe2G <liangdepeng@gmail.com>
2025-08-08 17:14:10 +0800

    adapt musa

--- src/custom_controlnet_aux/depth_anything_v2/dpt.py
+++ src/custom_controlnet_aux/depth_anything_v2/dpt.py
@@ -214,7 +214,8 @@ class DepthAnythingV2(nn.Module):
         image = transform({'image': image})['image']
         image = torch.from_numpy(image).unsqueeze(0)
         
-        DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+        # DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+        DEVICE = 'musa' if torch.musa.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
         image = image.to(DEVICE)
         
         return image, (h, w)
