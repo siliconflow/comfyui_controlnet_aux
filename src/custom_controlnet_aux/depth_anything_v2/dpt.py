09650909320d6dd05bd97ef9a09262e272d1d4f2
Ldpe2G <liangdepeng@gmail.com>
2025-08-11 11:05:07 +0800

    refine

--- src/custom_controlnet_aux/depth_anything_v2/dpt.py
+++ src/custom_controlnet_aux/depth_anything_v2/dpt.py
@@ -214,8 +214,9 @@ class DepthAnythingV2(nn.Module):
         image = transform({'image': image})['image']
         image = torch.from_numpy(image).unsqueeze(0)
         
-        # DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
-        DEVICE = 'musa' if torch.musa.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+        DEVICE = 'cuda' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
+        if hasattr(torch, 'musa') and torch.musa.is_available():
+            DEVICE = 'musa'
         image = image.to(DEVICE)
         
         return image, (h, w)
